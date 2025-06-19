cdc8490f57577e76524006e512c270e14bd19ede
Bhardwajsimran <156330168+Bhardwajsimran@users.noreply.github.com>
2025-06-19 17:07:56 +0530

    Create BackendTasks.js

---
+++ BackendTasks.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const Task = require('../models/Task');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const tasks = await Task.find({ userId: req.userId });
+  res.json(tasks);
+});
+
+router.post('/', async (req, res) => {
+  const { title, description } = req.body;
+  const task = new Task({ userId: req.userId, title, description });
+  await task.save();
+  res.status(201).json(task);
+});
+
+router.put('/:id', async (req, res) => {
+  const updated = await Task.findOneAndUpdate(
+    { _id: req.params.id, userId: req.userId },
+    req.body,
+    { new: true }
+  );
+  res.json(updated);
+});
+
+router.delete('/:id', async (req, res) => {
+  await Task.findOneAndDelete({ _id: req.params.id, userId: req.userId });
+  res.json({ message: 'Task deleted' });
+});
+
+module.exports = router;
